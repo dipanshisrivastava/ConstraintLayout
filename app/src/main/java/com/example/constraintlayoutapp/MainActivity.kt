f1707518dc37c26d72cc35a02f05756a5c9c490b
Dipanshi Srivastava <sd.dipanshi@gmail.com>
2023-08-21 14:32:46 +0530

    change user commit

--- app/src/main/java/com/example/constraintlayoutapp/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayoutapp/MainActivity.kt
@@ -29,7 +29,7 @@ private lateinit var binding: ActivityMainBinding
 
     }
 override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    // Inflate the menu; this adds items to the action bar if it is present.
+    // Inflate the menu; This adds items to the action bar if it is present.
     menuInflater.inflate(R.menu.menu_main, menu)
     return true
 }
